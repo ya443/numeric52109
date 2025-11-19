fd60c081df005fc5ef3be679cfda38c47f0e4044
Yasvanti Akilakulasingam <Yasvanti.Akilakulasingam@Yasvantis-MacBook-Pro.local>
2025-11-19 21:03:36 +0000

    edited the __init__.py file to make simple_package importable

--- simple_package/__init__.py
+++ simple_package/__init__.py
@@ -1,1 +1,10 @@
 # Init file for the package
+
+# Export functions from operations.py
+from .operations import *
+
+# Export functions from statistics.py
+from .statistics import *
+
+# Export functions from graphics.py
+from .graphics import *
